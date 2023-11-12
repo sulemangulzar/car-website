596f17622dd2b3bd3fe2ad457a07f2f49d08fa2f
sulemangulzar <sulemangulzar07@gmail.com>
2023-11-12 10:36:24 +0500

    changed

---
+++ main.js
@@ -0,0 +1,27 @@
+const navMenu = document.getElementById('nav-menu')
+const navToggle = document.getElementById('nav-toggle')
+const navClose = document.getElementById('nav-close')
+
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+if (navToggle) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.remove('hidden')
+    })
+}
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+if (navClose) {
+    navClose.addEventListener('click', () => {
+        navMenu.classList.add('hidden')
+    })
+}
+/*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction() {
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('hidden')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
